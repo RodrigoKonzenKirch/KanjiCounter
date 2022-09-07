261c596f4bf329a0ebb795d109699b4f9110df8e
Rodrigo Kirch <rodrigokirch@gmail.com>
2022-09-07 15:45:58 -0300

    Initial commit

---
+++ app/src/main/java/com/example/kanjicounter/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.kanjicounter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.kanjicounter.ui.theme.KanjiCounterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val viewModel: MainActivityViewModel by viewModels()
+
+        setContent {
+            KanjiCounterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    AppScreen(viewModel)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AppScreen(viewModel: MainActivityViewModel) {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    val scroll = rememberScrollState(0)
+
+    val charsCountedToUi = viewModel.getCharactersCounted().observeAsState()
+
+    Column(modifier = Modifier.padding(8.dp)) {
+
+        Column {
+            TextField(
+                value = text,
+                onValueChange = { newText ->
+                    text = newText
+                    viewModel.updateText(newText.text)
+                    viewModel.charactersCountedUpdate()
+                },
+                modifier = Modifier
+                    .weight(1F)
+                    .fillMaxSize()
+                    .padding(vertical = 8.dp),
+                placeholder = { Text(stringResource(R.string.textPlaceholder))}
+            )
+
+            Text(
+                text = charsCountedToUi.value ?: "",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .weight(1F)
+                    .fillMaxSize()
+                    .verticalScroll(scroll)
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kanjicounter/MainActivityViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.kanjicounter
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+    private var _text = MutableLiveData<String>()
+    private var _charactersCounted = MutableLiveData<String>()
+
+    init {
+        _text.value = ""
+        _charactersCounted.value = ""
+    }
+
+    fun updateText(newText: String){
+        _text.value = newText
+    }
+
+    fun getCharactersCounted(): LiveData<String> {
+        return _charactersCounted
+    }
+
+    fun charactersCountedUpdate() {
+
+        val textNotDuplicated = removeDuplicates(_text.value!!)
+
+        val mapOfCharsNumberOfOccurrences = countOccurrencesOfCharInText(textNotDuplicated, _text.value!!)
+
+        val orderedMapOfCharsNumberOfOccurrences = mapOfCharsNumberOfOccurrences.entries.sortedByDescending { it.value }.associate { it.toPair() }
+
+        var valuesFormattedIntoString = ""
+        for (item in orderedMapOfCharsNumberOfOccurrences){
+            valuesFormattedIntoString += "${item.key} ${item.value}\n"
+        }
+
+        _charactersCounted.value = valuesFormattedIntoString
+    }
+
+    private fun removeDuplicates(text:String): MutableSet<Char>{
+        val charsWithNoDuplicates = mutableSetOf<Char>()
+
+        for (char in text){
+            charsWithNoDuplicates.add(char)
+        }
+
+        return charsWithNoDuplicates
+    }
+
+    private fun countOccurrencesOfCharInText(setOfChars: Set<Char>, text: String): MutableMap<Char, Int>{
+        val mapOfCharsNumberOfOccurrences = mutableMapOf<Char, Int>()
+
+        for (char in setOfChars){
+            mapOfCharsNumberOfOccurrences.put(char, text.count{ it == char})
+        }
+
+        return mapOfCharsNumberOfOccurrences.toSortedMap()
+    }
+}
