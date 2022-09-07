9fdeb83c867578a1fcc8ff4f54f4aeb19fe5374d
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2022-09-07 19:19:54 -0300

    Updated UI color

--- app/src/main/java/com/example/kanjicounter/ui/theme/Color.kt
+++ app/src/main/java/com/example/kanjicounter/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.example.kanjicounter.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val lightGrey200 = Color(0xFFBDBDBD)
+val lightGrey500 = Color(0xFFA7C0CD)
+val lightGrey700 = Color(0xFF78909C)
 val Teal200 = Color(0xFF03DAC5)
