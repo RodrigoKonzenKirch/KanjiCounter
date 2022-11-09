8e4ad164d85f40d55ed1791236b1a166e2dfd3e0
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2022-11-08 23:01:20 -0300

    Updated build.gradle

---
+++ app/build.gradle.kts
@@ -0,0 +1,71 @@
+import java.net.InetAddress.getByName
+
+plugins {
+    id ("com.android.application")
+    id ("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk =32
+
+    defaultConfig {
+        applicationId = "com.example.kanjicounter"
+        minSdk = 21
+        targetSdk = 32
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    val composeVersion = "1.1.1"
+
+    implementation ("androidx.core:core-ktx:1.7.0")
+    implementation ("androidx.compose.ui:ui:$composeVersion")
+    implementation ("androidx.compose.material:material:$composeVersion")
+    implementation ("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    implementation ("androidx.compose.runtime:runtime-livedata:1.2.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation ("androidx.activity:activity-compose:1.3.1")
+
+    testImplementation ("org.junit.jupiter:junit-jupiter:5.8.0")
+    testImplementation ("androidx.arch.core:core-testing:2.1.0")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+    debugImplementation ("androidx.compose.ui:ui-tooling:$composeVersion")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest:$composeVersion")
+}
