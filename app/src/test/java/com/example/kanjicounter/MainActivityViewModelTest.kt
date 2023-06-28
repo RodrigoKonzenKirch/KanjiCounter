5bde1dc0f4c04e025cfb57b974436ea9b10331ed
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2023-06-28 11:08:02 -0300

    Updated Gradle plugin.

--- app/src/test/java/com/example/kanjicounter/MainActivityViewModelTest.kt
+++ app/src/test/java/com/example/kanjicounter/MainActivityViewModelTest.kt
@@ -2,8 +2,7 @@ package com.example.kanjicounter
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.kanjicounter.ui.mainscreen.CharCounter
-import com.example.kanjicounter.ui.mainscreen.MainActivityViewModel
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import com.example.kanjicounter.ui.mainscreen.MainScreenViewModel
 import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.jupiter.api.Assertions.*
@@ -14,7 +13,7 @@ internal class MainActivityViewModelTest {
     @get:Rule
     var rule = InstantTaskExecutorRule()
 
-    private val viewModel = MainActivityViewModel()
+    private val viewModel = MainScreenViewModel()
 
     @Test
     fun updateCharacters() = runBlocking{
@@ -23,7 +22,7 @@ internal class MainActivityViewModelTest {
 
         viewModel.updateText(input)
         viewModel.updateCharacters()
-        assertEquals(expected, viewModel.charactersCounted.value)
+        assertEquals(expected, viewModel.charactersCounted.value!![0])
 
     }
 }
